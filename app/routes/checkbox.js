8c5cb8ce8cf411acde3eae49d5fcaf9dd1cc5070
Carlo Gilmar <carlogilmar12@gmail.com>
2018-01-04 23:37:23 -0600

    Adding route for checkbox onchange!

--- app/routes/checkbox.js
+++ app/routes/checkbox.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    return [
+      { name: 'red', checked: false },
+      { name: 'yellow', checked: false },
+      { name: 'blue', checked: false }
+      ];
+  },
+
+  actions: {
+    notifyChecked: function(item) {
+			console.log(item.name);
+      alert(item.name + ": " + item.checked);
+    }
+  }
 });
