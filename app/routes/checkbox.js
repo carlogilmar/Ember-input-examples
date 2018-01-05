86c52803b36a8f6c92f7005f7797603c4b4da24b
Carlo Gilmar <carlogilmar12@gmail.com>
2018-01-04 23:59:45 -0600

    Addiong imple on change for add models

--- app/routes/checkbox.js
+++ app/routes/checkbox.js
@@ -3,16 +3,22 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(){
     return [
-      { name: 'red', checked: false },
-      { name: 'yellow', checked: false },
-      { name: 'blue', checked: false }
+			{ username:'carlogilmar1', profile:'teacher1', campus:'GDL1'},
+			{ username:'carlogilmar2', profile:'teacher2', campus:'GDL2'},
+			{ username:'carlogilmar3', profile:'teacher3', campus:'GDL3'},
+			{ username:'carlogilmar4', profile:'teacher4', campus:'GDL4'}
       ];
   },
 
   actions: {
-    notifyChecked: function(item) {
-			console.log(item.name);
-      alert(item.name + ": " + item.checked);
-    }
+    notifyChecked: function(user) {
+			console.log(user.username);
+    },
+
+		getList:function(){
+			console.log("Agaarro la lista aqui");
+			var list = this.get('usersForLoad');
+			console.log(list);
+		}
   }
 });
